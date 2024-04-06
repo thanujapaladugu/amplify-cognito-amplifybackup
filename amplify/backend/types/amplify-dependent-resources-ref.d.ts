21dc5940cc3b66ae9e80775d041403f7badfe9e9
TP997 <pthanuja997.com>
2024-04-06 00:48:58 -0700

    initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "mygameapp9b7ca1fe": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
